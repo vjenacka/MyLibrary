43efc326d3de069887234d0596fb160a329d2cbf
vjenacka <edins613@gmail.com>
2019-10-06 15:17:58 +0200

    added initial routes and layout template, need to fix the config for heroku

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
-const PORT=process.env.PORT || 3000;
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.on("error", error => console.error(error));
+db.once("open", () => console.log("Connected to Mongoose!"));
+
+const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+
+//routers
+const indexRouter = require("./routes/index");
+app.use("/", indexRouter);
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
